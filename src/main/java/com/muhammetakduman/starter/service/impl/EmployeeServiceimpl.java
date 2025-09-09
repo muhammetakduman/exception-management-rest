0c3df47eaefd95aad0fc0d61b77c8cb07df9fa78
Muhammet Akduman <akdumanmxc@gmail.com>
2025-09-10 01:14:52 +0300

    add to service thow

--- src/main/java/com/muhammetakduman/starter/service/impl/EmployeeServiceimpl.java
+++ src/main/java/com/muhammetakduman/starter/service/impl/EmployeeServiceimpl.java
@@ -4,6 +4,9 @@ import com.muhammetakduman.starter.dto.DtoDepartment;
 import com.muhammetakduman.starter.dto.DtoEmployee;
 import com.muhammetakduman.starter.entitiy.Department;
 import com.muhammetakduman.starter.entitiy.Employee;
+import com.muhammetakduman.starter.exception.BaseException;
+import com.muhammetakduman.starter.exception.ErrorMessage;
+import com.muhammetakduman.starter.exception.MessageType;
 import com.muhammetakduman.starter.repository.EmployeeRepository;
 import com.muhammetakduman.starter.service.IEmployeeService;
 import org.springframework.beans.BeanUtils;
@@ -22,10 +25,13 @@ public class EmployeeServiceimpl implements IEmployeeService {
         DtoEmployee dtoEmployee = new DtoEmployee();
         DtoDepartment dtoDepartment = new DtoDepartment();
 
+
         Optional<Employee> optional = employeeRepository.findById(id);
+
         if (optional.isEmpty()){
-            return null;
+            throw  new BaseException(new ErrorMessage(MessageType.NO_RECORD_EXIST,id.toString()));
         }
+
         Employee employee = optional.get();
         Department department =  employee.getDepartment();
 
