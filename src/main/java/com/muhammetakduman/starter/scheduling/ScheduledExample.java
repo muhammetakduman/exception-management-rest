73bc82e096b13b1e1fdeaa7984d4fc4467cec183
Muhammet Akduman <akdumanmxc@gmail.com>
2025-09-11 00:01:04 +0300

    junit5 byİd Test

--- src/main/java/com/muhammetakduman/starter/scheduling/ScheduledExample.java
+++ src/main/java/com/muhammetakduman/starter/scheduling/ScheduledExample.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class ScheduledExample {
-    @Scheduled(cron = "0/5 * * * * *")
+    //@Scheduled(cron = "0/5 * * * * *")
     private void arttirma(){
         for (int i = 1; i < 10; i++) {
             System.out.print(i + " ");
--- src/test/java/com/muhammetakduman/exception_management/ExceptionManagementApplicationTests.java
+++ src/test/java/com/muhammetakduman/exception_management/ExceptionManagementApplicationTests.java
@@ -1,13 +1,22 @@
 package com.muhammetakduman.exception_management;
 
+import com.muhammetakduman.starter.ExceptionManagementStarter;
+import com.muhammetakduman.starter.dto.DtoEmployee;
+import com.muhammetakduman.starter.service.IEmployeeService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = {ExceptionManagementStarter.class})
 class ExceptionManagementApplicationTests {
+	@Autowired
+	private IEmployeeService iEmployeeService;
 
 	@Test
-	void contextLoads() {
+	public void TestFindEmployeeById(){
+		 DtoEmployee dtoEmployee = iEmployeeService.findEmployeeById(2L);
+		 if (dtoEmployee != null){
+			 System.out.println("İsim" + dtoEmployee.getName());
+		 }
 	}
-
 }
