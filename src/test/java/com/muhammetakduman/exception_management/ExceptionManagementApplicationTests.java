0349b439e7e86785596671ada2974483528c1dac
Muhammet Akduman <akdumanmxc@gmail.com>
2025-09-11 00:17:05 +0300

    beforeeach , aftereach(methoddan sonra)

--- src/test/java/com/muhammetakduman/exception_management/ExceptionManagementApplicationTests.java
+++ src/test/java/com/muhammetakduman/exception_management/ExceptionManagementApplicationTests.java
@@ -3,6 +3,8 @@ package com.muhammetakduman.exception_management;
 import com.muhammetakduman.starter.ExceptionManagementStarter;
 import com.muhammetakduman.starter.dto.DtoEmployee;
 import com.muhammetakduman.starter.service.IEmployeeService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,11 +14,21 @@ class ExceptionManagementApplicationTests {
 	@Autowired
 	private IEmployeeService iEmployeeService;
 
+	@BeforeEach
+	public void beforeEach(){
+		System.out.println("junit teste hoşgeldiliniz sistem başlatılıyor.");
+	}
+
 	@Test
 	public void TestFindEmployeeById(){
 		 DtoEmployee dtoEmployee = iEmployeeService.findEmployeeById(2L);
 		 if (dtoEmployee != null){
 			 System.out.println("İsim" + dtoEmployee.getName());
 		 }
+	}
+
+	@AfterEach()
+	public void afterEach(){
+		System.out.println("After Each çalıştı");
 	}
 }
